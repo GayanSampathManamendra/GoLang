ee388450d501dec7e93eb10071752ffd1cfc1ede
GayanSampathManamendra <gayansampathmanamendra@gmail.com>
2020-02-02 21:27:07 +0530

    variables in golang

---
+++ variables.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+			"fmt"
+		
+)
+
+func main(){
+	
+		fmt.Println("Variables in golang")
+		
+		var number1 = 100
+		
+		var number2 int = 200
+		
+		var number3 float64 = 58.67
+		
+		var s = "hello world !"
+		
+		number5 , number6 := 9 , 10
+		
+		fmt.Println(number1)
+		fmt.Println(number2)
+		fmt.Println(number3)
+		fmt.Println(s)
+		fmt.Println(number5,number6)
+}
