2d0a1f633bee43e49352ca0af6c7e85b6436b496
GayanSampathManamendra <gayansampathmanamendra@gmail.com>
2020-02-02 21:52:51 +0530

    constants in golang

---
+++ constants.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+
+			const pi = 3.14
+			
+			fmt.Println(pi)
+			
+			// can't be re assign new number to the particular constant value...
+			// pi=5.34
+			
+			var number int = 45
+			
+			fmt.Println(number)
+			
+			// can reassign an another number ...
+			number=55
+			
+			fmt.Println(number)
+}
