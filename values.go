04a2c0ffae591c5495e132bdec282fcba3f3102b
GayanSampathManamendra <gayansampathmanamendra@gmail.com>
2020-02-02 20:57:15 +0530

    values in golang

---
+++ values.go
@@ -0,0 +1,18 @@
+package main
+
+import ( 
+			"fmt"
+			"time"
+		)
+
+func main(){
+
+		fmt.Println("This is values tutorial !");
+		
+		fmt.Println(1+1)
+		fmt.Println(2-1)
+		fmt.Println(2*1)
+		fmt.Println(5%2)
+		
+		fmt.Println("Time is now :",time.Now())
+}
